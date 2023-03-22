fdc39e00350cc4d195f05d450b8a7b0e2d113f7b
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-22 22:13:57 +0100

    chore: add status endpoint

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 
 import accounts from './accounts'
 import auth from './auth'
+import status from './status'
 
 const router = Router()
 
+router.use('/status', status)
 router.use('/accounts', accounts)
 router.use('/auth', auth)
 
