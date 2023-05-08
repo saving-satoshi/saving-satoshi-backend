83ff9e88f13923bcd24d79e581f3f6f6595ddff5
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-05-08 20:51:22 +0200

    fix: make sure progress always returns

--- src/routes/v1/progress/get.ts
+++ src/routes/v1/progress/get.ts
@@ -8,12 +8,23 @@ const router = Router()
 router.get('/', authenticated, async (req: RequestWithToken, res) => {
   try {
     if (!(await Progress.exists('account', req.account.id))) {
-      throw new Error('Progress not found.')
+      const entry = await Progress.create(
+        { account: req.account.id, progress: 'CH1INT1' },
+        { uniqueOn: 'account' }
+      )
+
+      return res.status(200).json({
+        account: entry.account,
+        progress: entry.progress
+      })
+    } else {
+      const entry = await Progress.find({ account: req.account.id })
+
+      res.status(200).json({
+        account: entry.account,
+        progress: entry.progress
+      })
     }
-
-    const progress = await Progress.find({ account: req.account.id })
-
-    res.status(200).json(progress)
   } catch (err) {
     res.status(500).json({
       errors: [
