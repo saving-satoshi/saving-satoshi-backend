96bac2cc533a5f57b583553c81f91619d7b011d3
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-06-02 15:16:38 +0200

    feat: repls - rename vm to repl

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,7 +4,7 @@ import accounts from './accounts'
 import auth from './auth'
 import status from './status'
 import progress from './progress'
-import vm from './vm'
+import repl from './repl'
 
 const router = Router()
 
@@ -12,6 +12,6 @@ router.use('/status', status)
 router.use('/accounts', accounts)
 router.use('/auth', auth)
 router.use('/progress', progress)
-router.use('/vm', vm)
+router.use('/repl', repl)
 
 export default router
