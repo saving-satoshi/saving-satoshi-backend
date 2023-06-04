55c655a1909ff95b3ad7f39fbddb02fab782a8b6
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-06-04 20:36:28 +0200

    refactor: sse to websockets

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import { cors } from 'middleware'
+import * as http from 'http'
+import * as WebSocket from 'ws'
+
+import * as repl from 'lib/repl'
 
 import { v1 } from './routes'
 
@@ -12,6 +16,21 @@ const port = process.env.PORT
 
 async function run() {
   const app = express()
+  const server = http.createServer(app)
+  const wss = new WebSocket.Server({ server })
+
+  wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', async (message: string) => {
+      const { action, payload } = JSON.parse(message)
+      switch (action) {
+        case 'repl': {
+          const id = await repl.prepare(payload.code, payload.language)
+          await repl.run(id, payload.language, ws)
+          break
+        }
+      }
+    })
+  })
 
   app.use(cors)
   app.use(bodyParser.json())
@@ -19,7 +38,7 @@ async function run() {
 
   app.use('/v1', v1)
 
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`listening on http://localhost:${port}`)
   })
 }
