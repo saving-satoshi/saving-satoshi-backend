34cb4afc7ba5cdbae3ec45e166795ed6ebfc1899
Samson Olusegun <olusegunsamson@live.com>
2023-08-29 08:51:57 +0100

    migrate progress route to prisma schema

--- src/routes/v1/progress/get.ts
+++ src/routes/v1/progress/get.ts
@@ -1,29 +1,36 @@
-import { Router } from 'express'
-import { Progress } from 'models'
-import { authenticated } from 'middleware'
-import { RequestWithToken } from 'types'
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { authenticated } from 'middleware';
+import { RequestWithToken } from 'types';
 
-const router = Router()
+const router = Router();
+const prisma = new PrismaClient();
 
 router.get('/', authenticated, async (req: RequestWithToken, res) => {
   try {
-    if (!(await Progress.exists('account', req.account.id))) {
-      const entry = await Progress.create(
-        { account: req.account.id, progress: 'CH1INT1' },
-        { uniqueOn: 'account' }
-      )
+    // Use Prisma to check if a Progress entry exists for the authenticated account
+    const progressEntry = await prisma.accounts_progress.findFirst({
+      where: { account: req.account.id }
+    });
+
+    if (!progressEntry) {
+      // If no entry exists, create a new one
+      const newProgressEntry = await prisma.accounts_progress.create({
+        data: {
+          account: req.account.id,
+          progress: 'CH1INT1',
+        },
+      });
 
       return res.status(200).json({
-        account: entry.account,
-        progress: entry.progress
-      })
+        account: newProgressEntry.account,
+        progress: newProgressEntry.progress,
+      });
     } else {
-      const entry = await Progress.find({ account: req.account.id })
-
       res.status(200).json({
-        account: entry.account,
-        progress: entry.progress
-      })
+        account: progressEntry.account,
+        progress: progressEntry.progress,
+      });
     }
   } catch (err) {
     res.status(500).json({
@@ -32,8 +39,11 @@ router.get('/', authenticated, async (req: RequestWithToken, res) => {
           message: err.message,
         },
       ],
-    })
+    });
+  } finally {
+    await prisma.$disconnect(); // Disconnect from the database
   }
-})
+});
 
-export default router
+export default router;
+
--- src/routes/v1/progress/set.ts
+++ src/routes/v1/progress/set.ts
@@ -1,26 +1,37 @@
-import { Router } from 'express'
-import { Progress } from 'models'
-import { authenticated } from 'middleware'
-import { RequestWithToken } from 'types'
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { authenticated } from 'middleware';
+import { RequestWithToken } from 'types';
 
-const router = Router()
+const router = Router();
+const prisma = new PrismaClient();
+
 
 router.put('/', authenticated, async (req: RequestWithToken, res) => {
   try {
-    if (await Progress.exists('account', req.account.id)) {
-      const progress = await Progress.update(
-        { progress: req.body.progress },
-        { account: req.account.id }
-      )
+    // Check if progress exists for the authenticated account
+    const existingProgress = await prisma.accounts_progress.findFirst({
+      where: { account: req.account.id },
+    });
 
-      res.status(200).json(progress)
+    if (existingProgress) {
+      // Update the existing progress
+      const updatedProgress = await prisma.accounts_progress.update({
+        where: { id: existingProgress.id },
+        data: { progress: req.body.progress },
+      });
+
+      res.status(200).json(updatedProgress);
     } else {
-      const progress = await Progress.create(
-        { account: req.account.id, progress: req.body.progress },
-        { uniqueOn: 'account' }
-      )
+      // Create a new progress record
+      const newProgress = await prisma.accounts_progress.create({
+        data: {
+          account: req.account.id,
+          progress: req.body.progress,
+        },
+      });
 
-      res.status(200).json(progress)
+      res.status(200).json(newProgress);
     }
   } catch (err) {
     res.status(500).json({
@@ -29,8 +40,11 @@ router.put('/', authenticated, async (req: RequestWithToken, res) => {
           message: err.message,
         },
       ],
-    })
+    });
+  } finally {
+    await prisma.$disconnect(); // Disconnect from the database
   }
-})
+});
 
-export default router
+export default router;
+
