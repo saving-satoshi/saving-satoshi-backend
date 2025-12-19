7606dc6e635352fb18aabf5016ece2b043fb9dc5
Se7enZ <se7enZ@protonmail.com>
2025-12-19 15:29:15 +0100

    chore(infra): Send application logs to CloudWatch.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,8 @@
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "${local.namespace}-app"
+  retention_in_days = 1
+}
+
 resource "aws_security_group" "app" {
   name = "${local.namespace}-app"
 }
@@ -29,11 +34,39 @@ resource "aws_security_group_rule" "app_allow_api" {
   security_group_id = aws_security_group.app.id
 }
 
+resource "aws_iam_role" "app_instance" {
+  name = "${local.namespace}-app-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.app_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_instance_profile" "app_instance" {
+  name = "${local.namespace}-app-instance-profile"
+  role = aws_iam_role.app_instance.name
+}
+
 resource "aws_instance" "app" {
-  ami             = var.ami
-  instance_type   = var.instance_size
-  key_name        = var.key_pair_name
-  security_groups = [aws_security_group.app.name]
+  ami                  = var.ami
+  instance_type        = var.instance_size
+  key_name             = var.key_pair_name
+  security_groups      = [aws_security_group.app.name]
+  iam_instance_profile = aws_iam_instance_profile.app_instance.name
 }
 
 # Add an Elastic IP (EIP) so replacing instances will maintain the
